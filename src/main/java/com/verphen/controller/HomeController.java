7a822bab0c2d36f3fff3d5ace7640e725deb4910
verphen <verphen@163.com>
2014-06-11 19:18:32 +0800

    add jscop js

--- src/main/java/com/verphen/controller/HomeController.java
+++ src/main/java/com/verphen/controller/HomeController.java
@@ -5,14 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/picture")
 public class HomeController {
-	public ModelAndView handleRequest(HttpServletRequest request,
+	
+	@RequestMapping("/test")
+	public String test(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		System.out.println("-------Ω¯»ÎHelloSpringMVCController¿‡--------");
-		return new ModelAndView("/hello");
+		
+		
+		
+		
+		
+		return "success";
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,28 @@
+<%@page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
 <body>
-<h2>spring MVC</h2>
+
+	<br>
+	<br>
+
+	<a href="/springDemo/picture/test">图片剪切</a>
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+	<form enctype="multipart/form-data" method="post">
+
+		<input type="file">
+
+	</form>
+
+	<img alt="Preview" src="resources/imgs/pic.jpg" />
 </body>
 </html>
+
+<script src="resources/js/jquery.min.js"></script>
+<script src="resources/js/jquery.Jcrop.js"></script>
