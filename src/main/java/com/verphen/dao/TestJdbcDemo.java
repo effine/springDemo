ff35c9311268f0d09498f10f591dffb3209cea45
verphen <verphen@163.com>
2014-06-25 15:59:14 +0800

    update

---
+++ src/main/java/com/verphen/dao/TestJdbcDemo.java
@@ -0,0 +1,14 @@
+/**
+ * @author verphen
+ * @date 2014年6月25日  下午3:30:37
+ */
+
+package com.verphen.dao;
+
+import java.util.List;
+
+import com.verphen.model.Student;
+
+public interface TestJdbcDemo {
+	public List<Student> getStuList();
+}
